56b08347a3f6fcec465514de6f434d62d8f6ce9a
holyocheese <543296676@qq.com>
2019-06-16 22:46:54 +0900

    fix

--- holycheese-ocr/ocr-platform-service/src/main/java/tesseract/Example.java
+++ holycheese-ocr/ocr-platform-service/src/main/java/tesseract/Example.java
@@ -8,16 +8,18 @@ import static org.bytedeco.leptonica.global.lept.*;
 public class Example {
 
 	public static void main(String[] args) {
+		
 		BytePointer outText;
 
         TessBaseAPI api = new TessBaseAPI();
-        if (api.Init("D:\\cheese java\\ocr\\holycheese-ocr\\ocr-platform-service\\src\\main\\resources\\traindata", 
+        if (api.Init("D:/cheese java/ocr/ocr-cheese/holycheese-ocr/ocr-platform-service/src/main/resources/traindata", 
         		"eng") != 0) {
             System.err.println("Could not initialize tesseract.");
             System.exit(1);
         }
 
-        PIX image = pixRead(args.length > 0 ? args[0] : "D:\\1.png");
+        PIX image = pixRead(args.length > 0 ? args[0] : 
+        	"D:\\cheese java\\ocr\\ocr-cheese\\holycheese-ocr\\ocr-platform-service\\src\\main\\resources\\pic\\1.png");
         api.SetImage(image);
         outText = api.GetUTF8Text();
         System.out.println("OCR output:\n" + outText.getString());
